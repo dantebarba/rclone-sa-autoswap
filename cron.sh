87eef12d7dc8908c15ae710b821b1591514504f8
Dante Barba <dantebarba@Dantes-iMac.local>
2020-10-03 15:02:13 -0300

    switch to crond

--- cron.sh
+++ cron.sh
@@ -6,14 +6,14 @@ if [ -z "$CRON" ]
     /app/replace.sh $CONFIG_DIR $PREFIX $MAX_NUMBER
   else
     # Setup cron schedule
+    crontab -d
     echo "$CRON /app/replace.sh $CONFIG_DIR $PREFIX $MAX_NUMBER >> /var/log/replace.log 2>&1" > /tmp/crontab.tmp
     crontab /tmp/crontab.tmp
-    crontab -l
     rm /tmp/crontab.tmp
     # Start cron
     echo "INFO: Starting cron ..."
     touch /var/log/crond.log
-    cron -f &
+    crond -b -l 0 -L /var/log/crond.log
     echo "INFO: cron started"
     tail -F /var/log/crond.log /var/log/replace.log
   fi

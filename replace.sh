1c9cde246d6a19eb5cfc55e287f0ecd74426fe33
Dante Barba <dantebarba@Dantes-iMac.local>
2020-10-04 13:57:52 -0300

    bash declare

--- replace.sh
+++ replace.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "$(date) INFO: Swap replace start";
+sa_number=$(( ( RANDOM % $3 )  + 1 ));
+echo "$(date) INFO: SA selected is: $2$sa_number.json";
 cp $1 $1.bak
-sed -i "/service_account_file = /c\service_account_file = $2$(( ( RANDOM % $3 )  + 1 )).json" $1.bak;
+sed -i "/service_account_file = /c\service_account_file = $2$sa_number.json" $1.bak;
 cat $1.bak > $1;
 rm $1.bak;
